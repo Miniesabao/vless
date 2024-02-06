2cd0c686e050550b1205819ce7a9972538487d5b
Miniesabao <159140870+Miniesabao@users.noreply.github.com>
2024-02-06 19:46:24 +0700

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -5,7 +5,7 @@ import { connect } from 'cloudflare:sockets';
 // [Windows] Press "Win + R", input cmd and run:  Powershell -NoExit -Command "[guid]::NewGuid()"
 let userID = 'cc2eca53-d1d4-40fd-ab90-ded58bc97956';
 
-const proxyIPs = ['202.159.35.121'];
+const proxyIPs = ['34.124.151.124;54.254.5.130;4.193.39.202;172.188.34.226'];
 
 let proxyIP = proxyIPs[Math.floor(Math.random() * proxyIPs.length)];
 
